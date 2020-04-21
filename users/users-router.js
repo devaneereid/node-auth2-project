42a5a432f05e515820521e426248bb7a3ae56116
Devanee Reid <devaneereid@gmail.com>
2020-04-21 12:38:18 -0600

    added knexfile, users, and set up migrations

---
+++ users/users-router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const Users = require('./users-model.js');
+
+// GET USERS
+router.get('/', (req, res) => {
+    console.log('token', req.decodedToken);
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send(error);
+        });
+});
+
+module.exports = router;
